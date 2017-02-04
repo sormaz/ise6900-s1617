306a8c9696d5d75d556b48b491e297726570e299
cd-tsa-ohio <sormaz@ohio.edu>
2017-02-03 23:58:04 -0500

    new update implemented about 70 % of commands. Not implemented commands give message.

--- project3/src/edu/ohio/ise/ise6900/model/Machine.java
+++ project3/src/edu/ohio/ise/ise6900/model/Machine.java
@@ -1,10 +1,25 @@
 package edu.ohio.ise.ise6900.model;
 
+import java.util.Collection;
+
 public class Machine extends MfgObject {
+	
+	Collection<AbstractState> states;
 
 	public Machine(String n) {
 		super(n);
 		// TODO Auto-generated constructor stub
+	}
+
+	public void addState(AbstractState a) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Method addState() not implemented yet");
+	}
+	
+	@Override
+	public void printout() {
+		throw new UnsupportedOperationException("Method printout() not implemented yet");
+		
 	}
 
 }
